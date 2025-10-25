5b7b74f5a8282b48957a7c4468d0b123d5fbc058
Alexey Elizarov <alex.elizarov1@gmail.com>
2025-10-25 22:57:59 +0300

    feat: docs meta and icons

--- apps/docs/src/app/layout.tsx
+++ apps/docs/src/app/layout.tsx
@@ -1,6 +1,12 @@
 import { RootProvider } from "fumadocs-ui/provider/next";
 import "./global.css";
+import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+
+export const metadata: Metadata = {
+  title: "React MVVM",
+  description: "An implementation of MVVM for React applications with MobX.",
+};
 
 const inter = Inter({
   subsets: ["latin"],
